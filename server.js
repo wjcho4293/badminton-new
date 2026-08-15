e1ab553725888809e6082093720992b3c961f45b
wjcho4293 <wjcho4293@gmail.com>
2026-08-16 02:44:11 +0900

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+const session = require('express-session');
 const SQLiteStore = require('connect-sqlite3')(session);
 
 app.use(session({
